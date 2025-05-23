3b58f585ad040f606cecd11db4d2dbcc7cced0ea
Sunni Xue <sunni.xue@gmail.com>
2025-05-22 22:07:32 -0400

    messed up material ui - fixed

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import logo from '../assets/turtle.png';
+import {Link} from 'react-router-dom';
+import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
   return (
@@ -7,7 +10,18 @@ function Navbar() {
       <div className='left'>
         <img src={logo} alt="Logo" style={{ width: '100px' }} />
       </div>
-      <div className='right'></div>
+
+      <div className='right'>
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        
+        <button>
+          <ReorderIcon/>
+        </button>
+
+      </div>
     </div>
   );
 }
