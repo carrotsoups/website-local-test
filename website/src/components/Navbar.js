3ad96b3a7163a5c39e8617ecef3c73e20185952b
Sunni Xue <sunni.xue@gmail.com>
2025-05-23 00:13:29 -0400

    navbar works

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assets/turtle.png';
 import {Link} from 'react-router-dom';
 import '../styles/Navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [openLinks,  setOpenLinks] = useState(false);
+
+  const toggleNavbar = () =>{
+      setOpenLinks(!openLinks)
+  }
+
   return (
     <div className='navbar'>
       <div className='left'>
-        <img src={logo} alt="Logo" style={{ width: '100px' }} />
+        <img src={logo} alt="Logo"/>
+
+        <div className='hiddenLinks' id={openLinks?"open":"close"}>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+        </div>
+        
       </div>
 
       <div className='right'>
@@ -17,7 +31,7 @@ function Navbar() {
         <Link to="/contact">Contact</Link>
         <Link to="/about">About</Link>
         
-        <button>
+        <button onClick={toggleNavbar}>
           <ReorderIcon/>
         </button>
 
--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "../styles/Home.css"
+import {Link} from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      hello world
+    <div className='Home'>
+      <div className='headerContainer'>
+        <h1>Sunni Xue</h1>
+        <p>hi im description</p>
+
+        <Link to="/menu">
+          <button>Github</button>
+        </Link>
+        
+      </div>
     </div>
   )
 }
