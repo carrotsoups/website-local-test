85692a6eec8d63c07dce7b296f7e6cffdccc7687
Sunni Xue <sunni.xue@gmail.com>
2025-05-24 20:54:58 -0400

    typing + text

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -9,8 +9,13 @@ function Home() {
     <div className='Home'>
       <div className='headerContainer'>
         <div className='left'>
-          <h1>Sunni Xue</h1>
-          <p>hi im description</p>
+          <h1 className='glow'>Hi I'm Sunni Xue</h1>
+          <div className='box'>
+            <div>
+              <p className='typing'>hi im description</p>
+            </div>
+          </div>
+      
 
           <Link to="https://github.com/carrotsoups" target="_blank"><button>Github</button></Link>
         </div>
